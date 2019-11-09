bb57dc9e56a96413c9a33e183f46c3cb07828ce7
Wang Luo <55168186+WwLuo-1024@users.noreply.github.com>
2019-11-09 17:12:19 +0800

    Create get_link.java

---
+++ src/main/java/com/stiw3054/get_link.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+/**
+ *
+ * @author WANG LUO
+ */
+//This class is used for get every follower link,and we just need to replace each log id of link,then we can get each follower page link.
+public class get_link {
+    String name;
+    public get_link(String name){
+        this.name = name;
+    }
+        
+        public String getLinksByJsoup(){  
+        String full_link;
+        String link = "https://github.com/{}?&tab=followers";
+        full_link=link.replace("{}", name);       
+        return full_link;
+    }
+}
